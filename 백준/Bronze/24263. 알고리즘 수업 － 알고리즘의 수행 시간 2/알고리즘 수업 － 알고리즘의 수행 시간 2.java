96029265c565c120267f772758f81408d71af884
rmk-3850 <qop589@naver.com>
2025-12-28 23:10:09 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+			
+			StringBuilder sb=new StringBuilder();
+			int n=Integer.parseInt(br.readLine().trim());
+			sb.append(String.valueOf(n)).append(System.lineSeparator()).append(String.valueOf(1));
+			bw.write(sb.toString());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
