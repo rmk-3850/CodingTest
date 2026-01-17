aa89f99b808a94e5617c442349e658626d3ccef6
rmk-3850 <qop589@naver.com>
2026-01-17 21:22:39 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+			int N=Integer.parseInt(br.readLine());
+			bw.write(String.valueOf(pibo(N)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	static int pibo(int n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return pibo(n-1)+pibo(n-2);
+	}
+}
